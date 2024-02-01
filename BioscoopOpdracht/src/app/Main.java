c8d161b57810a8e805018c4e27bd8895ff401b2f
Spike Strous <spike.strous@gmail.com>
2024-02-01 19:35:10 +0100

    Refactor price calculation

--- BioscoopOpdracht/src/app/Main.java
+++ BioscoopOpdracht/src/app/Main.java
@@ -9,7 +9,7 @@ public class Main {
         System.out.println("Hello world!");
 
         Movie movie = new Movie("The Matrix");
-        MovieScreening movieScreening = new MovieScreening(LocalDateTime.now(), 10.0, movie);
+        MovieScreening movieScreening = new MovieScreening(LocalDateTime.now().plusDays(1), 10.0, movie);
         MovieTicket ticket = new MovieTicket(3, 18, true, movieScreening);
         MovieTicket ticket2 = new MovieTicket(3, 19, true, movieScreening);
         MovieTicket ticket3 = new MovieTicket(3, 20, true, movieScreening);
@@ -18,7 +18,7 @@ public class Main {
         MovieTicket ticket6 = new MovieTicket(3, 23, true, movieScreening);
 
 
-        Order order = new Order(1, false);
+        Order order = new Order(1, true);
         order.addSeatReservation(ticket);
         order.addSeatReservation(ticket2);
         order.addSeatReservation(ticket3);
